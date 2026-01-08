2d2a5a5025bff2246fb62c9c5440158693a30770
sumathi3355 <sumathi.s2023@vitstudent.ac.in>
2026-01-08 17:08:02 +0530

    update the changes

--- file1.py
+++ file1.py
@@ -1,2 +1,17 @@
-print("hello world")
-print("Hi guys")
+books = []
+
+def add_book(name):
+    books.append(name)
+    print("Book added")
+
+def view_books():
+    if not books:
+        print("No books available")
+    else:
+        for b in books:
+            print(b)
+
+add_book("Python Basics")
+add_book("Data Structures")
+
+view_books()
