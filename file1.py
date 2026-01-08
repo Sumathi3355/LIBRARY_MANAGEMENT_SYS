b791d173b674821b086b1979bda48306f2a44cd6
sumathi3355 <sumathi.s2023@vitstudent.ac.in>
2026-01-08 18:38:17 +0530

    commit this

--- file1.py
+++ file1.py
@@ -8,7 +8,7 @@ if username == "admin":
         print("Login successful")
         print("Welcome to Library System")
     else:
-        print("Wrong password")
+        print("Incorrect password")
 else:
     print("Invalid user")
 
